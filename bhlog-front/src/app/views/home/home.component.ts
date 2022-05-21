b36a913bbab5cfcf2311689b41f2d82c335fe9f5
Gustavo Barros <gustavo.pereira@ihm.com.br>
2022-05-21 17:42:38 -0300

    Home component finished

--- bhlog-front/src/app/views/home/home.component.ts
+++ bhlog-front/src/app/views/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { PhotosView, PostsView } from 'app/shared/utils/views.utils';
 
 @Component({
   templateUrl: './home.component.html',
@@ -6,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
   }
 
+  navigateToPosts(){
+    this.router.navigate([`/${PostsView.url}`])
+  }
+
+  navigateToPhotos(){
+    this.router.navigate([`/${PhotosView.url}`])
+  }
 }
