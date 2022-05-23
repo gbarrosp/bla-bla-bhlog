c739a306447b6317b01c224d49c0e15ff8ad2825
Gustavo Barros <gustavo.pereira@ihm.com.br>
2022-05-23 15:35:43 -0300

    Java project added

---
+++ bhlog-back/src/main/java/com/bhlog/bhlogback/BhlogBackApplication.java
@@ -0,0 +1,13 @@
+package com.bhlog.bhlogback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BhlogBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BhlogBackApplication.class, args);
+	}
+
+}
---
+++ bhlog-back/src/test/java/com/bhlog/bhlogback/BhlogBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bhlog.bhlogback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BhlogBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
